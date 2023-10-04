aa178403fa1eed123f3565c84ee1612573104565
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2023-10-04 18:50:22 +0300

    213
    213

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,12 @@
   const swiper = new Swiper('.swiper', {
     // Optional parameters
     // direction: 'vertical',
-    // loop: true,
+    loop: true,
+    delay: 5000,
+    enabled: true,
+    fill: "column",
+    rows: 4,
+    crossFade: true,
   
     // If we need pagination
     // pagination: {
@@ -24,6 +29,7 @@
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    
   
     // And if we need scrollbar
     // scrollbar: {
@@ -59,13 +65,19 @@
   btnReserv.addEventListener('click' , function() {
     document.querySelector('.reservation-modal').classList.toggle('visible-item')
     document.body.classList.toggle('body-active') 
+    document.querySelectorAll('.container').forEach(function(e) {
+      e.classList.add('opacity-body')
+    })
   })
 
   btnClose.addEventListener('click' , function() {
     document.querySelector('.reservation-modal').classList.remove('visible-item')
     document.body.classList.remove('body-active') 
+    document.querySelectorAll('.container').forEach(function(e) {
+      e.classList.remove('opacity-body')
+    })
   })
-
+ 
   
 
 
@@ -113,12 +125,18 @@
   
   menuAll.addEventListener('click' , function() {
     itemMenu.forEach(function(e) {
+      document.querySelector('.kitchen-menu').classList.remove('swiper')
+      document.querySelector('.swiper-btns').style.display = 'none';
+      document.querySelector('.swiper-wrapper').classList.add('row') 
       e.classList.toggle('visible-item')
       if(e.classList.toggle('visible-item"'))
       {
         menuAll.textContent = 'Скрыть'
       } else {
         menuAll.textContent = 'Всё меню'
+      document.querySelector('.kitchen-menu').classList.add('swiper')
+      document.querySelector('.swiper-btns').style.display = 'block';
+      document.querySelector('.swiper-wrapper').classList.remove('row') 
       }
     })
   })
@@ -154,6 +172,13 @@ tabItem.forEach(function (e) {
   e.addEventListener('click' , function(){
     e.classList.toggle('tabs-block-active')
     // target.classList.toggle('wrapper-list-hidden')
+    tabList.forEach(function(i) {
+      i.classList.toggle('wrapper-list-hidden')
+      if(tabItem.classList('tabs-block-active'))
+      {
+        tabList.classList.toggle('wrapper-list-hidden')
+      }
+    })
   }) 
 })
 
